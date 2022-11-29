f49a5889acbd297622d94e39cb65f566f441119d
Mubarak Tijani <66437072+Tijani-Mubarak2001@users.noreply.github.com>
2022-11-29 13:55:30 +0100

    changed file name to app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st
 from PIL import Image
 
-loaded_model = pickle.load(open('C:/Users/temp/AI Credit Card Fraud detection proj/trained_fraud_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_fraud_model.sav', 'rb'))
 
 
 # creating a function for prediction
